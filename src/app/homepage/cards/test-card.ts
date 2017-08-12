d11a32331c44f4228cba3904fdc2470c76deb412
Adam Klein <kleina3@carleton.edu>
2017-08-12 14:35:51 -0400

    configured bootstrap to more or less work

--- src/app/homepage/cards/test-card.ts
+++ src/app/homepage/cards/test-card.ts
@@ -1,11 +1,20 @@
 import { Card } from './../../content-card/card';
 
 export const testCard: Card = { 
-	title: "Loaded Test Card 1",
+	title: "What is Hexrust Mesa?",
 	
-	topContent: "<p>text testing for a <i>new</i> test card</p>",
+	topContent: "<p>The world of Hexrust Mesa is a personal campaign setting created for the D&D 5th edition roleplaying game. This website acts as a guide to the world and an interactive campaign log of the activities of the characters who participate in these games.</p>",
 	
 	dateMade: "2017-8-7",
 	
-	accordion: [["Category Title 1", "Category content 1"], ["Title 2", "Content 2"]]
+	accordion: [
+    [
+    "World details and overview", 
+    "<p>Hexrust Mesa is a fantasy world that, 200 years ago, suffered a catastrophe that threw it into the realm of Mad Max or the Wild West</p>"
+    ], 
+    [
+    "Feel",
+    "<p>The world of Hexrust Mesa is gritty in almost every sense of the term. The possibility of death hangs over every adventuring party, and players will often be faced with situations where there is no ideal solution. Victory will not be handed to you; it must be earned.</p><p>On the other hand, the opportunity for heroism is plentiful. The world is rife with evils that actively terrorize the common folk. Player characters will usually be seeking to enact positive change, not just prevent negative ones.</p>"
+    ]
+    ]
 };
