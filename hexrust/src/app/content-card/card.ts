104c0b9377a6209ef37b2aa3389001298073c802
Adam Klein <kleina3@carleton.edu>
2017-08-11 20:48:17 -0400

    removed extra files

--- hexrust/src/app/content-card/card.ts
+++
@@ -1,8 +0,0 @@
-export class Card {
-	title: string;
-	//inserts html
-	topContent: string;
-	dateMade: string;
-	//first string is section title, second string is section content (html)
-	accordion: [string, string][];
-}
--- hexrust/src/app/home.ts
+++
@@ -1,1 +0,0 @@
-export const jumboTitle: string = "Title Change Test";
